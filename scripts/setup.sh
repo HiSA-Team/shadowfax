7e3efa60ce569b1b64d104e2a72f32d0d58e4cd4
Giuseppe Capasso <46749018+alarmfox@users.noreply.github.com>
2025-03-10 10:30:48 +0100

    Environment setup depending on linux distribution (#2)
    Environment setup with `script/setup.sh`
    - Extended documentation about dependencies
    - Added the `setup.sh` script to install dependencies.

---
+++ scripts/setup.sh
@@ -0,0 +1,34 @@
+ # This file installs dependencies for Linux systems. Distribution name is retrieved using lsb_release.
+ # Users must make sure lsb_release is installed on their system before running this script.
+ # Author:  Giuseppe Capasso <capassog97@gmail.com>
+#!/bin/sh
+
+if [ "$(id -u)" -ne 0 ] || [ ! $SUDO_USER ]; then
+  echo "This script must be run as root with sudo, not directly as root" >&2
+  exit 1
+fi
+
+USER_NAME="$SUDO_USER"
+echo "Running the script as $USER_NAME"
+
+# setup dependencies
+DISTRO_CODENAME=$(lsb_release -c | awk '{print $2}')
+
+case "$DISTRO_CODENAME" in
+  noble | jammy)
+    apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install make qemu-system \
+      gcc-riscv64-linux-gnu
+    ;;
+  bookworm | bullseye)
+    apt update && DEBIAN_FRONTEND=noninteractive apt -y install make qemu-system \
+      gcc-riscv64-linux-gnu
+    ;;
+  void)
+    xbps-install -Sy qemu make cross-riscv64-linux-gnu
+    ;;
+  *)
+    echo "Unsupported distribution: $DISTRO_CODENAME" >&2
+    echo "Make sure you install dependencies according to your distribution."
+    exit 1
+    ;;
+esac
