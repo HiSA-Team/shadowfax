eb6619959545718f0179965709fed426406d8dfa
giuseppe <capassog97@gmail.com>
2025-08-02 16:05:20 +0200

    Fix commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,26 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'shadowfax'
+project = "shadowfax"
 copyright = "2025, Giuseppe Capasso"
 author = "Giuseppe Capasso"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx_copybutton",
-    "sphinx_rtd_dark_mode",
-    "sphinx_toolbox.collapse"
-]
+extensions = ["sphinx_copybutton", "sphinx_rtd_dark_mode", "sphinx_toolbox.collapse"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
