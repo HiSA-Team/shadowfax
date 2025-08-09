997151e809bbcec993167af6b60a99e413953f1b
giuseppe <capassog97@gmail.com>
2025-08-09 12:39:52 +0200

    added comment in build.rs

--- build.rs
+++ build.rs
@@ -18,8 +18,8 @@ use std::{env, fs};
 const PLATFORM_BASE: &str = "platform";
 
 fn main() {
+    // Ensure the bin/ folder exists.
     fs::create_dir_all("bin").unwrap();
-    // output directory
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let opensbi_path = env::var("OPENSBI_PATH").map(PathBuf::from).expect("OPENSBI_PATH must be set; run `source environment.sh <opensbi-path>` or set OPENSBI_PATH in your environment");
 
