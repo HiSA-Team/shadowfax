5fcd81f64b335f96aaf41689602625cbf7e9cb35
giuseppe <capassog97@gmail.com>
2025-09-24 23:24:26 +0200

    feat: handling global state

--- payload/tsm/memory.x
+++ payload/tsm/memory.x
@@ -1,24 +1,42 @@
 MEMORY
 {
-  FLASH (rx) : ORIGIN = 0x0, LENGTH = 1M
-  BOOT_RAM (rw) : ORIGIN = 0x00100000, LENGTH = 1M
+    /* Use a generic base address - will be relocated by ELF loader */
+    RAM (rwx) : ORIGIN = 0x0, LENGTH = 1M
 }
 
-REGION_ALIAS("REGION_TEXT", FLASH);
-REGION_ALIAS("REGION_STACK", BOOT_RAM);
-
-_b_stack_size = 0x2000;
+REGION_ALIAS("REGION_TEXT", RAM);
+REGION_ALIAS("REGION_DATA", RAM);
+REGION_ALIAS("REGION_BSS", RAM);
 
 SECTIONS {
+    . = ORIGIN(RAM);
 
-    .text : ALIGN(4k) {
+    .text : ALIGN(4K) {
         KEEP(*(._start));
-        . = ALIGN(4K);
         *(.text .text.*);
+        *(.rodata .rodata.*);
     } > REGION_TEXT
 
-    .boot_stack (NOLOAD) : ALIGN(4K) {
-        . += _b_stack_size;
+    .data : ALIGN(8) {
+        *(.data .data.*);
+        *(.sdata .sdata.*);
+    } > RAM
+
+    .bss : ALIGN(8) {
+        __bss_start = .;
+        *(.bss .bss.*);
+        *(.sbss .sbss.*);
+        __bss_end = .;
+    } > REGION_BSS
+
+    .stack (NOLOAD): ALIGN(4K) {
+        /* 8K stack */
+        . += 0x2000;
         _top_b_stack = .;
-    } > REGION_STACK
+    } > REGION_DATA
+
+    /* Add relocation sections */
+    .rela.text : { *(.rela.text*) }
+    .rela.data : { *(.rela.data*) }
+    .rela.bss : { *(.rela.bss*) }
 }
--- payload/tsm/src/main.rs
+++ payload/tsm/src/main.rs
@@ -2,7 +2,16 @@
 #![no_main]
 #![feature(fn_align)]
 
-use core::panic::PanicInfo;
+use core::{panic::PanicInfo, ptr::NonNull};
+
+mod state;
+use crate::state::{State, TsmInfo};
+
+#[repr(C)]
+struct SbiRet {
+    a0: isize,
+    a1: isize,
+}
 
 unsafe extern "C" {
     /// boot stack top (defined in `memory.x`)
@@ -20,14 +29,10 @@ fn panic(_info: &PanicInfo) -> ! {
 
 // Give each hart 8K stack
 const STACK_SIZE_PER_HART: usize = 1024 * 8;
+
 const SBI_COVH_GET_TSM_INFO: usize = 0;
 const SBI_COVH_CREATE_TVM: usize = 5;
-
-macro_rules! cove_unpack_fid {
-    ($fid:expr) => {
-        (($fid >> 26) & 0x3F, $fid & 0xFFFF)
-    };
-}
+const EID_COVH: usize = 0x434F5648;
 
 #[no_mangle]
 #[unsafe(naked)]
@@ -38,12 +43,16 @@ extern "C" fn _start() -> ! {
         // a0-a4 contains TEECALL parameters. We must preserve them
         r#"
         .attribute arch, "rv64imac"
+        // Save the a5 register (state address) into s9 to allow sp computation
+        mv s9, a5
+
         add a5, zero, zero
         li t0, {stack_size_per_hart}
         mul t1, a5, t0
         la sp, {stack_top}
         sub sp, sp, t1
 
+        mv a5, s9
         call {main}
         "#,
 
@@ -53,50 +62,32 @@ extern "C" fn _start() -> ! {
     )
 }
 
-struct Guest {
-    vcpu_state: [u64; 32],
-}
-struct State {
-    info: TsmInfo,
-    guest: Option<Guest>,
-}
-
-impl State {
-    fn new() -> Self {
-        Self {
-            info: TsmInfo {
-                tsm_state: TsmState::TsmReady,
-                tsm_impl_id: 69,
-                tsm_version: 0,
-                tsm_capabilities: 0,
-                tvm_state_pages: 0,
-                tvm_max_vcpus: 1,
-                tvm_vcpu_state_pages: 0,
-            },
-            guest: None,
-        }
-    }
-}
-
-struct SbiRet {
-    a0: isize,
-    a1: isize,
-}
-
 // Since this is a TSM with non reentrant model, an ECALL should be a TEERET
-fn main(a0: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> ! {
-    let mut a6: usize;
-    unsafe { core::arch::asm!("add {}, a6, zero", out(reg) a6, options(nomem)) };
-    let (_sdid, fid) = cove_unpack_fid!(a6);
-    let state = State::new();
+fn main(
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+    a7: usize,
+) -> ! {
+    // The TSM should be called only for CoVH
+    assert_eq!(a7, EID_COVH);
+
+    let fid = a6 & 0xFF;
+    let state = unsafe {
+        FirmwareState::from_addr(a5).expect("firmware didn't provide state pointer in a5")
+    };
+
     let ret = match fid {
         SBI_COVH_GET_TSM_INFO => {
-            let tsm_info_ptr = a0 as *mut TsmInfo;
-            let info = state.info.clone();
+            let info = state.info_clone();
 
             assert_eq!(a1, core::mem::size_of::<TsmInfo>());
             unsafe {
-                tsm_info_ptr.write(info);
+                core::ptr::write(a0 as *mut TsmInfo, info);
             }
             SbiRet {
                 a0: 0,
@@ -116,95 +107,60 @@ fn main(a0: usize, a1: usize, a2: usize, a3: usize, a4: usize) -> ! {
             in("a0") ret.a0,
             in("a1") ret.a1,
             in("a6") a6,
-            in("a7") 0x434F5648,
+            in("a7") EID_COVH,
             options(noreturn)
         );
     };
 }
-
-#[repr(C)]
-#[derive(Clone, Debug)]
-pub struct TsmInfo {
-    /*
-     * The current state of the TSM (see `tsm_state` enum above).
-     * If the state is not `TSM_READY`, the remaining fields are invalid and
-     * will be initialized to `0`.
-     */
-    pub tsm_state: TsmState,
-    /*
-     * Identifier of the TSM implementation, see `Reserved TSM Implementation IDs`
-     * table below. This identifier is intended to distinguish among different TSM
-     * implementations, potentially managed by different organizations, that might
-     * target different deployment models and, thus, implement subset of CoVE spec.
-     */
-    pub tsm_impl_id: u32,
-    /*
-     * Version number of the running TSM.
-     */
-    pub tsm_version: u32,
-    /*
-     * A bitmask of CoVE features supported by the running TSM, see `TSM Capabilities`
-     * table below. Every bit in this field corresponds to a capability defined by
-     * `COVE_TSM_CAP_*` constants. Presence of bit `i` indicates that both the TSM
-     * and hardware support the corresponding capability.
-     */
-    pub tsm_capabilities: usize,
-    /*
-     * The number of 4KB pages which must be donated to the TSM for storing TVM
-     * state in sbi_covh_create_tvm_vcpu(). `0` if the TSM does not support the
-     * dynamic memory allocation capability.
-     */
-    pub tvm_state_pages: usize,
-    /*
-     * The maximum number of vCPUs a TVM can support.
-     */
-    pub tvm_max_vcpus: usize,
-    /*
-     * The number of 4KB pages which must be donated to the TSM when creating
-     * a new vCPU. `0` if the TSM does not support the dynamic memory allocation
-     * capability.
-     */
-    pub tvm_vcpu_state_pages: usize,
-}
-/*
- * TsmPageType is an enumeration that defines the types of memory pages supported by the TSM.
- * It includes options for 4 KiB, 2 MiB, 1 GiB, and 512 GiB pages, allowing for flexible memory
- * management and allocation.
- */
-pub enum TsmPageType {
-    /* 4 KiB */
-    Page4k = 0,
-    /* 2 MiB */
-    Page2mb = 1,
-    /* 1 GiB */
-    Page1gb = 2,
-    /* 512 GiB */
-    Page512gb = 3,
+/// Small typed wrapper around the firmware-provided pointer.
+///
+/// All `unsafe` pointer casting happens in `from_a5()`; the rest of the code
+/// uses safe methods where possible.
+pub struct FirmwareState {
+    ptr: NonNull<State>,
 }
 
-/*
- * TvmState is an enumeration that represents the state of a Trusted Virtual Machine (TVM).
- * It indicates whether the TVM is in the process of initialization or is ready to run.
- */
-#[derive(Clone)]
-pub enum TvmState {
-    /* The TVM has been created, but isn't yet ready to run */
-    TvmInitializing = 0,
-    /* The TVM is in a runnable state */
-    TvmRunnable = 1,
-}
+impl FirmwareState {
+    /// Try to build `FirmwareState` from the register `a5`.
+    ///
+    /// This performs basic checks (non-null and alignment). It's `unsafe` because
+    /// we cannot guarantee the pointed memory has the correct provenance or lifetime.
+    pub unsafe fn from_addr(addr: usize) -> Option<Self> {
+        // read a5 into a usize
 
-/*
- * TsmState is an enumeration that describes the current state of the Trusted Software Module (TSM).
- * It provides information on whether the TSM is not loaded, loaded but not initialized, or fully
- * initialized and ready to accept ECALLs (environment calls).
- */
-#[derive(Clone, Debug)]
-pub enum TsmState {
-    /* TSM has not been loaded on this platform. */
-    TsmNotLoaded = 0,
-    /* TSM has been loaded, but has not yet been initialized. */
-    TsmLoaded = 1,
-    /* TSM has been loaded & initialized, and is ready to accept ECALLs.*/
-    TsmReady = 2,
+        if addr == 0 {
+            return None;
+        }
+
+        if addr % align_of::<State>() != 0 {
+            return None;
+        }
+
+        // Convert to NonNull; this is safe because addr != 0.
+        Some(Self {
+            ptr: NonNull::new_unchecked(addr as *mut State),
+        })
+    }
+
+    /// Obtain an immutable reference. This is safe only if no other party mutates
+    /// the structure concurrently. If the region can be mutated concurrently,
+    /// prefer `read_volatile` or explicit atomics.
+    pub fn as_ref(&self) -> &State {
+        unsafe { &*self.ptr.as_ptr() }
+    }
+
+    /// Obtain a mutable reference. This is `unsafe` because aliasing and concurrency
+    /// rules can be violated if another reference exists.
+    pub unsafe fn as_mut(&mut self) -> &mut State {
+        &mut *self.ptr.as_ptr()
+    }
+
+    /// Convenience: clone `info` field in a safe manner.
+    pub fn info_clone(&self) -> TsmInfo
+    where
+        TsmInfo: Clone,
+    {
+        // if info is not concurrently mutated, this is fine. Otherwise user must ensure sync.
+        self.as_ref().info.clone()
+    }
 }
